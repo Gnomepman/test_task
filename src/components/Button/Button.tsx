7c33be6c271ad77bede4e8378be495e6963f65e1
Danyil Derkach <64920477+Gnomepman@users.noreply.github.com>
2023-08-20 10:37:04 +0300

    feat: update button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { TaskStatus } from '@/types';
 import { Button, Typography } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 
@@ -8,8 +9,16 @@ const variants = {
   label: 'label',
 };
 
+export const mapButtonVariants: {
+  [key in TaskStatus]: { variant: keyof typeof variants; message: string };
+} = {
+  [TaskStatus.InProgress]: { variant: 'warning', message: 'В процесі' },
+  [TaskStatus.Completed]: { variant: 'success', message: 'Виконано' },
+  [TaskStatus.Missed]: { variant: 'error', message: 'Не вчасно' },
+};
+
 interface ButtonProps {
-  variant: keyof typeof variants | string;
+  variant: keyof typeof variants;
   children: React.ReactNode;
   onClick?: () => void;
 }
@@ -43,6 +52,7 @@ const useStyles = makeStyles()((theme) => {
     },
     [variants.label]: {
       background: theme.palette.background.paper,
+      color: theme.palette.common.black,
     },
   };
 });
